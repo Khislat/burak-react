54c0f752cb732429c7cad713f0871485721053e0
Khislat <khislatolimov29@gmail.com>
2025-01-11 05:51:42 -0800

    fix: develop burak screen based components

--- src/app/screens/Users.tsx
+++
@@ -1,5 +0,0 @@
-import { Container } from "@mui/material";
-
-export function Users() {
-    return <Container><h2>Users</h2></Container>;
-  }
---
+++ src/app/screens/homePage/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export function HomePage() {
+    return <Container><h2>HomePage</h2></Container>;
+}
